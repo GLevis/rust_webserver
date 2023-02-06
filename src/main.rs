9c2338b5df65a106458c8c75f488a737f946d758
Gavin Levis <levis.gavin@gmail.com>
2023-02-05 21:08:09 -0600

    changed hard coded html file to be whatever the user requests

--- src/main.rs
+++ src/main.rs
@@ -29,25 +29,37 @@ fn main() {
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
-
-    let (status_line, filename) = match &request_line[..] {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
-        "GET /sleep HTTP/1.1" => {
-            thread::sleep(Duration::from_secs(5));
-            ("HTTP/1.1 200 OK", "hello.html")
-        }
-        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
-    };
-
-    let contents = fs::read_to_string(filename).unwrap();
+fn handle_get(mut stream: TcpStream, file: &str) {
+    let mut chars = file.chars();
+    chars.next();
+    // TODO: error handling for file not existing
+    let contents = fs::read_to_string(chars.as_str()).unwrap();
     let length = contents.len();
-
+    let status_line = "HTTP/1.1 200 OK";
     let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+       format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
     stream.flush().unwrap();
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+     
+    if let request = &request_line[..].split(" ").collect::<Vec<&str>>() {
+        match request[0] {
+            "GET" => handle_get(stream, &request[1]),
+            _ => println!("no")
+        };
+    };
+
+    //let contents = fs::read_to_string(filename).unwrap();
+    //let length = contents.len();
+
+    //let response =
+     //   format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    //stream.write_all(response.as_bytes()).unwrap();
+    //stream.flush().unwrap();
 }
